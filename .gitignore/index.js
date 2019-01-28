d078a1a2d55f212950bd61224981440c154fe040
BestPaladin <45981792+BestPaladin@users.noreply.github.com>
2019-01-28 14:42:39 +1100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,5 +6,45 @@ client.login(process.env.TOKEN);
 
 client.on("ready", () => {
   console.log("PrincipaleBotGAME est allumé !");
-  client.user.setActivity("Test");
-}); 
+  client.user.setActivity("{:robot:} Je suis GamerBot");
+  function1();
+});
+
+var nom = process.env.NOM;
+var prefix = process.env.PREFIX;
+
+function function1() {
+  console.log("Jeu actualisé en '{🤖} Je suis "+nom+"'.");
+  client.user.setActivity("{🤖} Je suis " + nom);
+  setTimeout(function2, 5000);
+}
+
+function function2() {
+  console.log("Jeu actualisé en '{⚖} 3 serveurs principaux'.");
+  client.user.setActivity("{⚖} 3 serveurs principaux");
+  setTimeout(function3, 5000);
+}
+
+function function3() {
+  console.log("Jeu actualisé en '{🔨} BestPaladin#7883'.");
+  client.user.setActivity("{🔨} BestPaladin#7883");
+  setTimeout(function4, 5000);
+}
+
+function function4() {
+  console.log("Jeu actualisé en '{👀} Dovakid {Employé Xebot}#4404'.");
+  client.user.setActivity("{👀} Dovakid {Employé Xebot}#4404");
+  setTimeout(function5, 5000);
+}
+
+function function5() {
+  console.log("Jeu actualisé en '{🔑} préfix : "+ prefix +"'.");
+  client.user.setActivity("{🔑} préfix : "+ prefix);
+  setTimeout(function6, 5000);
+}
+
+function function6() {
+  console.log("Jeu actualisé en '{ℹ} "+prefix+"help'.");
+  client.user.setActivity("{ℹ} "+prefix+"help");
+  setTimeout(function1, 5000);
+}
